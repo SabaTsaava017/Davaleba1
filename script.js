45b4640ab63a3972f696bace0d7cde24a521b619
SabaTsaava017 <sabacaava7@gmail.com>
2023-09-12 18:25:38 +0400

    update

--- script.js
+++ script.js
@@ -48,4 +48,53 @@ const car = {
   const carAge = car.calculateAge();
 console.log(`ეს მანქანა არის ${carAge} წლის `);
 
+//5. დაწერეთ ფუნქცია რომელსაც გადავცემთ ციფრების მასივს,
+// მაგალითად [1,4,88,99, 123]. ფუნქციამ უნდა იპოვოს და დააბრუნოს
+// მასივის უდიდესი ციფრი
+
+
+
+// 7. დაწერეთ ფუნქცია რომელსაც გადასცემთ სიტყვას,
+// ფუნქციამ უნდა დააბრუნოს ამ სიტყვის შებრუნებული ვერსია,
+// მაგალითად თუ გადავცემთ someFn("ერთი"), უნდა დააბრუნოს "ითრე"
+
+function reverseWord(word) {
+    if (typeof word !== 'string') {
+      return "Input is not a valid string.";
+    }
+  
+    let reversed = '';
+    for (let i = word.length - 1; i >= 0; i--) {
+      reversed += word[i];
+    }
+  
+    return reversed;
+  }
+  
+  const originalWord = "saba";
+  const reversedWord = reverseWord(originalWord);
+  
+  console.log(`Original word: ${originalWord}`);
+  console.log(`Reversed word: ${reversedWord}`);
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
